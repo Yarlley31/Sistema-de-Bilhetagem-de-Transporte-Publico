9c1b569927f3f47e69f0fa523e23a5c0544617c6
Yarlley Fernandes <147113204+Yarlley31@users.noreply.github.com>
2025-03-13 17:57:00 -0300

    Implementação de comprar passagens

--- codigofonte_V2.py
+++ codigofonte_V2.py
@@ -16,63 +16,100 @@ while continuar == True:
     # categoria padrão
     if categoria_passageiro == 1:
         desconto = 0
-        funcionalidade = int(input("Selecione uma de nossas funcionalidades: \n[1] Recarga \n[2] Verificar saldo\n[3] Sair do sistema\n"))
+        funcionalidade = int(input("Selecione uma de nossas funcionalidades: \n[1] Recarga \n[2] Comprar passagens\n[3] Verificar saldo\n[4] Sair do sistema\n"))
         print("-" * 30)
+        
+        # faz a recarga
         if funcionalidade == 1:
             recarga = float(input("Digite o valor da recarga: R$"))
             saldo = recarga
-            valor_a_pagar = recarga
-            print(f"Você irá pagar R${valor_a_pagar:.2f}")
+            valor_da_recarga = recarga
+            print(f"Você irá pagar R${valor_da_recarga:.2f}")
             print("-" * 30)
-        
+            
+        # faz a compra de passagens
         elif funcionalidade == 2:
-            if saldo >= valor_a_pagar:
-                print(f"Você tem R${saldo:.2f} de saldo")
+            qnt_passagem = int(input("Quantas passagens você quer comprar: "))
+            valor_passagem *= qnt_passagem
+            if saldo >= valor_passagem:
+                print("Compra efetuada com sucesso!")
+            else:
+                print("Compra mal sucedida, devido a falta de saldo")
+                
+        # mostra o saldo
+        elif funcionalidade == 3:
+            if saldo >= valor_da_recarga:
+                print(f"Você tem R${saldo - valor_passagem} de saldo")
                 print("-" * 30)
             else:
                 print("Você não tem saldo suficiente!")
-        elif funcionalidade == 3:
+                
+        # sai do while
+        elif funcionalidade == 4:
             break
         
     # categoria estudante/idoso
     elif categoria_passageiro == 2:
         
-        funcionalidade = int(input("Selecione uma de nossas funcionalidades: \n[1] Recarga \n[2] Verificar saldo \n[3] Sair do sistema\n"))
+        funcionalidade = int(input("Selecione uma de nossas funcionalidades: \n[1] Recarga \n[2] Comprar passagens\n[3] Verificar saldo \n[4] Sair do sistema\n"))
+        
+        # faz a recarga
         if funcionalidade == 1:
             recarga = float(input("Digite o valor da recarga: R$"))
-            desconto = recarga / 2
-            valor_a_pagar = recarga - desconto
             saldo = recarga
-            print(f"Você irá pagar R${valor_a_pagar:.2f}")
+            print("Recarga efetuada com sucesso!")
+        
+        # faz a compra de passagens
         elif funcionalidade == 2:
-            if saldo >= valor_a_pagar:
-                print(f"Você tem R${saldo:.2f} de saldo")
-                print("-" * 30)
+            qnt_passagem = int(input("Quantas passagens você quer comprar: "))
+            valor_passagem = (valor_passagem / 2) * qnt_passagem 
+            if saldo >= valor_passagem:
+                print(f"Compra efetuada com sucesso!\nValor = R${valor_passagem:.2f}")
+                saldo -= valor_passagem
             else:
-                print("Você não tem saldo suficiente!")
+                print("Compra mal sucedida, devido a falta de saldo")
+        
+        # mostra o saldo
         elif funcionalidade == 3:
+            print(f"Você tem R${saldo:.2f} de saldo")
+        
+        # sai do while
+        elif funcionalidade == 4:
             break
     
     
     # categoria social
     elif categoria_passageiro == 3:
         
-        funcionalidade = int(input("Selecione uma de nossas funcionalidades: \n[1] Recarga \n[2] Verificar saldo\n[3] Sair do sistema\n"))
+        funcionalidade = int(input("Selecione uma de nossas funcionalidades: \n[1] Recarga \n[2] Comprar passagens\n[3] Verificar saldo\n[4] Sair do sistema\n"))
+        
+        # faz a recarga 
         if funcionalidade == 1:
             recarga = float(input("Digite o valor da recarga: R$"))
             desconto = recarga * (80/100)
-            valor_a_pagar = recarga - desconto
+            valor_da_recarga = recarga - desconto
             saldo = recarga
-            print(f"Você irá pagar R${valor_a_pagar:.2f}")
+            print(f"Você irá pagar R${valor_da_recarga:.2f}")
             
+        # faz a compra de passagens
         elif funcionalidade == 2:
-            if saldo >= valor_a_pagar:
+            qnt_passagem = int(input("Quantas passagens você quer comprar: "))
+            valor_passagem *= qnt_passagem
+            if saldo >= valor_passagem:
+                print("Compra efetuada com sucesso!")
+            else:
+                print("Compra mal sucedida, devido a falta de saldo")
+                
+        # mostra o saldo
+        elif funcionalidade == 3:
+            if saldo >= valor_da_recarga:
                 print(f"Você tem R${saldo:.2f} de saldo")
                 print("-" * 30)
             else:
                 print("Você não tem saldo suficiente!")
-        
-        elif funcionalidade == 3:
+
+        # sai do while
+        elif funcionalidade == 4:
             break
         
     # categoria saída
